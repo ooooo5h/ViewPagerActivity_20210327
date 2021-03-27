20ce679a5aae633ee9349a9dbcab3a3b1707fffd
전은형 <ooooo5h91@gmail.com>
2021-03-27 14:11:03 +0900

    페이지별 제목 할당

--- app/src/main/java/com/neppplus/viewpageractivity_20210327/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpageractivity_20210327/adapters/MainViewPagerAdapter.kt
@@ -9,6 +9,22 @@ import com.neppplus.viewpageractivity_20210327.fragments.Page3Fragment
 
 class MainViewPagerAdapter (fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
+//    position에 따른 페이지의 제목을 리턴하자
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        if (position == 0) {
+            return "인사"
+        }
+        else if(position == 1) {
+            return "자기소개"
+        }
+        else {
+            return "하고싶은말"
+        }
+    }
+
+
+
     override fun getItem(position: Int): Fragment {
 
 //        position의 값에 따라 다른 Fragment를 리턴할거야
