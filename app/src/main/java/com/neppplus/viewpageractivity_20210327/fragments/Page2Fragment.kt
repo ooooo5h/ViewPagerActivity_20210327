fa95806524dc61086bdca9c4546a1442d1e6ca34
전은형 <ooooo5h91@gmail.com>
2021-03-27 13:41:39 +0900

    프래그먼트 3개를 작성했다

---
+++ app/src/main/java/com/neppplus/viewpageractivity_20210327/fragments/Page2Fragment.kt
@@ -0,0 +1,19 @@
+package com.neppplus.viewpageractivity_20210327.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.viewpageractivity_20210327.R
+
+class Page2Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_page2, container, false)
+    }
+}
